bc1a928ab3cbd11c1afaa9d6048b0b1d69524f32
Melvin Baybayanon <melvsbaybayanon@gmail.com>
2021-08-04 00:29:10 +0800

    Initialize

---
+++ src/main/java/com/rakutech/rakutech/RakutechApplication.java
@@ -0,0 +1,13 @@
+package com.rakutech.rakutech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RakutechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RakutechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rakutech/rakutech/RakutechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakutech.rakutech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RakutechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
